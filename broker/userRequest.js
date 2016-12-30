a40c45cdadaf8c80139096f909850d279498fdf1
Tejasvi (Teju) Nareddy <tejunareddy@gmail.com>
2016-12-30 17:40:42 -0500

    Updated userRequest with sha1 verification

--- broker/userRequest.js
+++ broker/userRequest.js
@@ -4,9 +4,16 @@
 var logger = require('../util/logger');
 
 function messageReceived(req, res) {
-    // TODO
 
-    res.sendStatus(200);
+    if (req.headers['x-hub-signature'] == 'sha1=' + process.env.FB_APP_SECRET_SHA1) {
+        // Authenticated
+
+        // TODO
+        res.sendStatus(200);
+
+    } else {
+        res.status(403).send('Ok, you are not facebook..... stop it pls');
+    }
 }
 
 module.exports = messageReceived;

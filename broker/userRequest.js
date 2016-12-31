d2d2d4cce59fd7804b2108173d462443bba8d83b
Tejasvi (Teju) Nareddy <tejunareddy@gmail.com>
2016-12-30 20:59:33 -0500

    Fixed bug with sha1 header

--- broker/userRequest.js
+++ broker/userRequest.js
@@ -7,14 +7,13 @@ var security = require('../util/security');
 function messageReceived(req, res) {
 
     var sha1 = security.getSHA1WithAppSecret(JSON.stringify(req.body));
-    if (process.env.NODE_ENV == 'development' || req.headers['X-Hub-Signature'] == 'sha1' + sha1) {
+    if (process.env.NODE_ENV == 'development' || req.headers['x-hub-signature'] == 'sha1=' + sha1) {
         // Authenticated!
 
         res.sendStatus(200);
     } else {
         // sha1 headers didn't match... uh oh
 
-        logger.log('warn', 'post to broker with wrong sha1 ' + sha1);
         res.status(403).send('You are not facebook, are you? pls stop >:(');
     }
 }

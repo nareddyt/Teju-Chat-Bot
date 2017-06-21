3c67cbfadd6f6a43a8057b5f608b4ad2d872ca63
Tejasvi (Teju) Nareddy <tejunareddy@gmail.com>
2017-06-21 11:27:32 -0700

    Changed ping to verification token for now!

--- ping/ping.js
+++ ping/ping.js
@@ -3,7 +3,7 @@
 // File that contains the ping handler
 
 function handlePing(req, res) {
-    res.status(200).send('pong');
+    res.status(200).send('loaderio-fb617d85d8368ab570af9637375024ce');
 }
 
 module.exports = handlePing;
